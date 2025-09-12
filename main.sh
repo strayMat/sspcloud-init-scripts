b51f552c6a2597dacd7f1e2820a647024d19e5e1
Matthieu Doutreligne <m.doutreligne@proton.me>
2025-09-12 10:52:35 +0200

    git aliases
    add gp and gpf

--- main.sh
+++ main.sh
@@ -55,6 +55,8 @@ alias glp='_git_log_prettily'
 alias glg='git log --stat'
 alias glog='git log --oneline --decorate --graph'
 alias glgm='git log --graph --max-count=10'
+alias gp='git push'
+alias gpf='git push -f'
 EOF
 
 source ~/.bashrc
