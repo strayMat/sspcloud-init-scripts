6d44f54a20b2a9f49bb04ea873ac65be3608a805
Matthieu Doutreligne <m.doutreligne@proton.me>
2025-09-12 22:44:52 +0200

    main.sh: add code-server extensions

--- main.sh
+++ main.sh
@@ -23,6 +23,41 @@ jq '. + {
     "files.insertFinalNewline": true,  # Ensure files end with a newline
 }' "$SETTINGS_FILE" > "$SETTINGS_FILE.tmp" && mv "$SETTINGS_FILE.tmp" "$SETTINGS_FILE"
 
+#!/bin/sh
+
+# This init script install various useful VScode extensions
+# NB : only extensions from the Open VSX Registry (https://open-vsx.org/) can be installed on code-server
+# Expected parameters : None
+
+# CONFORT EXTENSIONS -----------------
+
+# Colorizes the indentation in front of text
+code-server --install-extension oderwat.indent-rainbow
+# Extensive markdown integration
+code-server --install-extension yzhang.markdown-all-in-one
+# Integrates Excalidraw (software for sketching diagrams)
+code-server --install-extension pomdtr.excalidraw-editor
+# Ruff extension for fast Python linter and code formatter
+code-server --install-extension charliermarsh.ruff
+# Roo code for agenting coding
+code-server --install-extension RooVeterinaryInc.roo-cline
+
+# COPILOT ----------------------------
+
+# Install Copilot (Microsoft's AI-assisted code writing tool)
+copilotVersion="1.234.0"
+copilotChatVersion="0.20.0" # This version is not compatible with VSCode server 1.92.2
+
+wget --retry-on-http-error=429 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/GitHub/vsextensions/copilot/${copilotVersion}/vspackage -O copilot.vsix.gz
+wget --retry-on-http-error=429 https://marketplace.visualstudio.com/_apis/public/gallery/publishers/GitHub/vsextensions/copilot-chat/${copilotChatVersion}/vspackage -O copilot-chat.vsix.gz
+
+gzip -d copilot.vsix.gz 
+gzip -d copilot-chat.vsix.gz 
+
+code-server --install-extension copilot.vsix
+code-server --install-extension copilot-chat.vsix
+rm copilot.vsix copilot-chat.vsix
+
 # Configure des raccourcis claviers personnalisés pour vscode
 # Add shortcuts
 echo '[
